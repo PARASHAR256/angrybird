5dc1ab86ffd4984c4d439a7bdee985a0140a8480
PARASHAR256 <parasharaw121@gmail.com>
2021-03-18 18:49:11 +0530

    parashar-created slingshot

---
+++ Slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+    }
+
+    display(){
+      //if the body a is not null then display the slingshot constraint
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);    
+       }
+           
+    }
+     fly(){
+     //removing the bird body to make it fly
+        this.sling.bodyA=null;   
+     }
+
+    
+}
